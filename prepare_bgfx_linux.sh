d2b731f538a3f992ddc710d5067c40b2c8b80a7d
Andrey Volodin <siddok@gmail.com>
2016-12-14 20:34:45 +0300

    Fix build script

--- prepare_bgfx_linux.sh
+++ prepare_bgfx_linux.sh
@@ -1,4 +1,7 @@
 cd 3rdparty/bgfx
+# You will need to manually add -fPIC flags to generated ninja files at this point
+# Bug has to be fixed in GENie project
+../bx/tools/bin/linux/genie --with-sdl --gcc=linux-gcc ninja
 ../bx/tools/bin/linux/ninja -C .build/projects/ninja-linux/release64 -t clean
 ../bx/tools/bin/linux/ninja -C .build/projects/ninja-linux/debug64 -t clean
 ../bx/tools/bin/linux/ninja -C .build/projects/ninja-linux/release64 bgfx
